ad2cbf624837bc03c101b67c59ac588d79c1a122
Direct-Leo <1713110887@qq.com>
2021-10-20 23:26:31 +0800

    update

--- ch10/ex10_17.cpp
+++ ch10/ex10_17.cpp
@@ -5,15 +5,13 @@
 #include "Sales_data.h"
 #include "Sales_data.cpp"
 
-bool compareIsbn(const Sales_data &sd1,const Sales_data &sd2)
-{
-    return (sd1.isbn() < sd2.isbn());
-}
+
 
 int main()
 {
     Sales_data sd1("java"),sd2("python"),sd3("C++");
     std::vector<Sales_data> v{sd1,sd2,sd3};
-    std::sort(v.begin(),v.end(),compareIsbn);
+    std::sort(v.begin(),v.end(),[](const Sales_data &lhs, const Sales_data &rhs)
+                                {return lhs.isbn() < rhs.isbn(); });
     return 0;
 }
